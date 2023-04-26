e207e6eca9081d855d4291f6bdc899bfad5fb093
Adrian Mincu <mal13adi03@gmail.com>
2023-04-26 14:36:58 +0300

    finished programming page

---
+++ JavaScript/programming.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    document.getElementById('link1').addEventListener('click', function() {
+        window.open('https://github.com/skpha13/University-Work/tree/main/First%20Year/Second%20Semester/Object-oriented%20programming/Labs/Project%202','_blank');
+    },false);
+
+    document.getElementById('link2').addEventListener('click', function() {
+        window.open('https://github.com/skpha13/Wordle-University-Project','_blank');
+    },false);
+
+    document.getElementById('link3').addEventListener('click', function() {
+        window.open('https://github.com/skpha13/University-Work','_blank')
+    },false);
+}
