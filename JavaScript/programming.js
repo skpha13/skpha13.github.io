a9ef2b6a0a0f9700cd152d3644cbd601a8bc5391
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-05-17 19:24:50 +0300

    changed link

--- JavaScript/programming.js
+++ JavaScript/programming.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     document.getElementById('link1').addEventListener('click', function() {
-        window.open('https://github.com/skpha13/University-Work/tree/main/First%20Year/Second%20Semester/Object-oriented%20programming/Labs/Project%202','_blank');
+        window.open('https://github.com/skpha13/Image-and-Video-Editing-Software','_blank');
     },false);
 
     document.getElementById('link2').addEventListener('click', function() {
